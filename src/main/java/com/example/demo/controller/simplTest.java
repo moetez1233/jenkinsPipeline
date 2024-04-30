ef5ecd208e933b0c4fb5edd5e87a0aee30640ae9
Moetez <moetezmaddouri@gmail.com>
2024-04-30 10:35:09 +0100

    fix web hook github

--- src/main/java/com/example/demo/controller/simplTest.java
+++ src/main/java/com/example/demo/controller/simplTest.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class simplTest {
     public ResponseEntity<String> getMessage(){
         return new ResponseEntity<>("hello Axeane team ", HttpStatus.OK);
     }
-    @PostMapping("github_webHook")
+    @PostMapping("/github_webHook")
     public ResponseEntity<String> verifWebHooks(){
         return new ResponseEntity<>("hello Axeane team ", HttpStatus.OK);
     }
